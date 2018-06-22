95087f3e1599ea807ec88946fd4fccf905e9f473
kdfwow64 <kdfwow64@gmail.com>
2018-06-22 15:54:18 +0000

    second commit

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,29 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/info', 'MainscrapingController@getdetailInfo');
+Route::get('/mail/send', 'MailController@send');
+Route::get('/mail/template', 'MailController@index');
+Route::get('/blacklist/manage', 'HomeController@blacklist');
+Route::get('/permission/manage', 'HomeController@permission');
+Route::get('/blacklist/delete/{id}', 'HomeController@blacklistDelete');
+
+Route::post('/home/scrape', 'MainscrapingController@searchwithKeyword');
+Route::post('/home/getDomains', 'HomeController@getDomains');
+Route::post('/home/getEmail', 'HomeController@getEmail');
+Route::post('/mail/sendAll', 'MailController@sendAll');
+Route::post('/mail/save', 'MailController@save');
+Route::post('/blacklist/insertD', 'HomeController@insertD');
+Route::post('/blacklist/insertE', 'HomeController@insertE');
+Route::post('/blacklist/insertN', 'HomeController@insertN');
+Route::post('/blacklist/getDomains', 'HomeController@getBlacklistDomains');
+Route::post('/blacklist/getEmails', 'HomeController@getBlacklistEmails');
+Route::post('/blacklist/getNames', 'HomeController@getBlacklistNames');
+Route::post('/permission/setPermission', 'HomeController@setPermission');
+
