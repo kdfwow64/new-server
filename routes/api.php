66d3f5eae37b5c415774dfd7541ac79458b0e3e1
kdfwow64 <kdfwow64@gmail.com>
2018-07-04 05:10:22 -0500

    for pull data3

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/scrape', 'MainscrapingController@APISearchwithKeyword');
+Route::get('/scrape', 'MainscrapingController@APISearchwithKeyword');
