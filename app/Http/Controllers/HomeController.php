7cc8d0689f9e4a6ca9ed98af6dfe4bb4c88a6041
kdfwow64 <kdfwow64@gmail.com>
2018-07-08 11:01:40 -0500

    fix api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,7 +47,7 @@ class HomeController extends Controller
         return view('home',compact('permission','flag'));
     }
 
-    public function isEnabledKeyword(Request $request) {
+    public function isEnabledKeyword_api(Request $request) {
         $temp  = Keyword::where('user_id','=',$request->input('id'))->orderBy('created_at','DESC')->get(['*'])->first();
         $flag = "";
         if($temp) {
@@ -61,6 +61,22 @@ class HomeController extends Controller
 
         echo $flag;
     }
+
+    public function addkeyword_api(Request $request) {
+        $new_keyword = new Keyword;
+        $new_keyword->keyword = $request->input('keyword');
+        $new_keyword->city = $request->input('keyword_city');
+        $new_keyword->state = $request->input('keyword_state');
+        $new_keyword->user_id = $request->input('id');
+        $new_keyword->flag = 0;
+        $saved = $new_keyword->save();
+
+        if($saved)
+            echo 1;
+        else
+            echo 0;
+    }
+
     public function addkeyword(Request $request) {
         $new_keyword = new Keyword;
         $new_keyword->keyword = $request->input('keyword');
@@ -75,6 +91,7 @@ class HomeController extends Controller
         else
             echo 0;
     }
+
     public function getDomains(Request $request) {
         $domain = $request->input('domain');
         $ss = '%'.$domain.'%';
--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/scrape', 'MainscrapingController@APISearchwithKeyword');
-Route::get('/addkeyword', 'HomeController@addkeyword');
-Route::get('/isEnabledKeyword', 'HomeController@isEnabledKeyword');
+Route::get('/addkeyword', 'HomeController@addkeyword_api');
+Route::get('/isEnabledKeyword', 'HomeController@isEnabledKeyword_api');
