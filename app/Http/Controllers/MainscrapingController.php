5cbc93cdedeabb1655be8f6a313b1a4fb2d117a9
kdfwow64 <kdfwow64@gmail.com>
2018-07-08 12:21:56 -0500

    third

--- app/Http/Controllers/MainscrapingController.php
+++ app/Http/Controllers/MainscrapingController.php
@@ -26,7 +26,7 @@ class MainscrapingController extends Controller
                 $flag = "";
         }
 
-        return response()->json($flag);
+        echo $flag;
     }
 
     public function addkeyword_api(Request $request) {
