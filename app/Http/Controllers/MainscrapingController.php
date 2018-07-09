5c89cf4908c1daf12e295e26b1e9ec69c8cff300
kdfwow64 <kdfwow64@gmail.com>
2018-07-09 01:25:06 -0500

    fix ajax return value7

--- app/Http/Controllers/MainscrapingController.php
+++ app/Http/Controllers/MainscrapingController.php
@@ -26,7 +26,7 @@ class MainscrapingController extends Controller
                 $flag = "";
         }
 
-        return response()->json($obj);
+        return response()->json($flag);
     }
 
     public function addkeyword_api(Request $request) {
