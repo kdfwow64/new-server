728ae06fb3d3cf5d933081ebb1a88031eb192eea
kdfwow64 <kdfwow64@gmail.com>
2018-07-10 00:06:23 -0500

    test222

--- app/Http/Controllers/MainscrapingController.php
+++ app/Http/Controllers/MainscrapingController.php
@@ -52,10 +52,9 @@ class MainscrapingController extends Controller
 
     public function getActive_api(Request $request) {
     	$id = $request->input('id');
-//        $active_data = Keyword::where('user_id','=',$request->input('id'))->get(['*']);
-    	$active_data = DB::raw("SELECT *, COUNT(if(flag = 1, 1, NULL)) as count_flag FROM (SELECT a.id, a.user_id, a.keyword, a.city, a.state,a.flag as status, b.flag FROM keywords a WHERE user_id = '$id' LEFT JOIN infos b ON a.id = b.keyword_id ) AS c GROUP BY id");
-    	var_dump($active_data);
-    	exit();
+        $active_data = Keyword::where('user_id','=',$request->input('id'))->get(['*']);
+/*    	$query = "SELECT *, COUNT(if(flag = 1, 1, NULL)) as count_flag FROM (SELECT a.id, a.user_id, a.keyword, a.city, a.state,a.flag as status, b.flag FROM keywords a WHERE user_id = '$id' LEFT JOIN infos b ON a.id = b.keyword_id ) AS c GROUP BY id";
+    	$active_data = DB::raw("SELECT *, COUNT(if(flag = 1, 1, NULL)) as count_flag FROM (SELECT a.id, a.user_id, a.keyword, a.city, a.state,a.flag as status, b.flag FROM keywords a WHERE user_id = '$id' LEFT JOIN infos b ON a.id = b.keyword_id ) AS c GROUP BY id");*/
         return response()->json($active_data);
     }
 
