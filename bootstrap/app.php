50b49c8dee83f731d510be0a9a488cd35a891dbb
Blue Parrot <blueparrot0815@gmail.com>
2018-07-28 10:39:41 +0200

    again back

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -51,7 +51,7 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
-
+/*
 $app->configure('cors');
 $app->register(Barryvdh\Cors\ServiceProvider::class);
 
@@ -62,5 +62,5 @@ $app->middleware([
 $app->routeMiddleware([
 	'cors' => \Barryvdh\Cors\HandleCors::class,
 ]);
-
+*/
 return $app;
